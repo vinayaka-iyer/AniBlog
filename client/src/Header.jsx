9a97ac22af299afcf7cc456bd3ef94a35b342d21
vinayaka-iyer <vinayakaiyer999@gmail.com>
2023-07-12 20:47:00 +0530

    UserContext and redirect

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 const Header = () => {
-	const [username, setUsername] = useState(null);
+	const { setUserInfo, userInfo } = useContext(UserContext);
 
 	useEffect(() => {
 		fetch("http://localhost:4000/profile", {
 			credentials: "include",
 		}).then((response) => {
 			response.json().then((userInfo) => {
-				setUsername(userInfo.username);
+				setUserInfo(userInfo);
 			});
 		});
 	}, []);
@@ -18,18 +19,21 @@ const Header = () => {
 			credentials: "include",
 			method: "POST",
 		});
-		setUsername(null);
+		setUserInfo(null);
 	}
+
+	const username = userInfo?.username;
+
 	return (
 		<header>
 			<Link to='/' className='logo'>
 				MyBlog
 			</Link>
 			{username && (
-				<>
+				<nav>
 					<Link to='/create'>Create New Post</Link>
 					<a onClick={logout}>Logout</a>
-				</>
+				</nav>
 			)}
 			{!username && (
 				<nav>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
-const LoginPage = () => {
+export default function LoginPage() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [redirect, setRedirect] = useState(false);
+	const { setUserInfo } = useContext(UserContext);
 	async function login(ev) {
 		ev.preventDefault();
 		const response = await fetch("http://localhost:4000/login", {
@@ -14,11 +16,15 @@ const LoginPage = () => {
 			credentials: "include",
 		});
 		if (response.ok) {
-			setRedirect(true);
+			response.json().then((userInfo) => {
+				setUserInfo(userInfo);
+				setRedirect(true);
+			});
 		} else {
-			alert("Wrong Credentials");
+			alert("wrong credentials");
 		}
 	}
+
 	if (redirect) {
 		return <Navigate to={"/"} />;
 	}
@@ -40,6 +46,4 @@ const LoginPage = () => {
 			<button>Login</button>
 		</form>
 	);
-};
-
-export default LoginPage;
+}
