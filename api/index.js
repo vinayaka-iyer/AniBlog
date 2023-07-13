ae7904babf87aa054c7286deb1e3515c31bbe7a9
vinayaka-iyer <vinayakaiyer999@gmail.com>
2023-07-13 10:53:04 +0530

    CreatePost page, Post Model, upload file Middleware and logic

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdf43q5421gretgadfag34t";
@@ -67,6 +71,22 @@ app.post("/logout", (req, res) => {
 		sameSite: "none",
 		secure: true,
 	}).json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+	const { originalname, path } = req.file;
+	const parts = originalname.split(".");
+	const ext = parts[parts.length - 1];
+	const newPath = path + "." + ext;
+	fs.renameSync(path, newPath);
+	const { title, summary, content } = req.body;
+	const PostDoc = await Post.create({
+		title,
+		summary,
+		content,
+		cover: newPath,
+	});
+	res.json(PostDoc);
 });
 
 app.listen(4000);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -20,7 +21,8 @@ function App() {
 				<Route path='/' element={<Layout />}>
 					<Route index element={<IndexPage />} />
 					<Route path={"/login"} element={<LoginPage />} />
-					<Route path={"/register"} element={<RegisterPage />} />
+					<Route path={"/register"} element={<RegisterPage />} />'
+					<Route path={"/create"} element={<CreatePost />} />
 				</Route>
 			</Routes>
 		</UserContextProvider>
